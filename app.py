3cc7ca1e2a9a16b77e34aa3be615619ef0455832
Ashly Maria P.S <asash9825@gmail.com>
2026-02-13 18:08:41 +0530

    code updated

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ app.secret_key = "secret_key"
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
+    conn.execute("PRAGMA foreign_keys = ON")
     return conn
 
 
